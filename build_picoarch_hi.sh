73c2add09d80f3613eae3210028ddea119a9479a
Tomasz Zubertowski <tzubertowski@gmail.com>
2026-09-09 21:38:48 +0200

    Link shared FrogUI settings helper in picoarch_hi

--- build_picoarch_hi.sh
+++ build_picoarch_hi.sh
@@ -25,11 +25,11 @@ DEFINES=(
 LDFLAGS="$CFLAGS -L$SYSROOT/usr/lib -lc -ldl -lgcc -lm -lSDL -lpng12 -lz -Wl,--gc-sections -lpthread -s"
 LDFLAGS="$LDFLAGS -Wl,-Ttext-segment=0x20000000"
 
-OBJS="libpicofe/input.o libpicofe/in_sdl.o libpicofe/linux/in_evdev.o libpicofe/linux/plat.o libpicofe/fonts.o libpicofe/readpng.o libpicofe/config_file.o cheat.o config.o content.o core.o menu.o menu_font.o i18n.o main.o options.o overrides.o patch.o scale.o scaler_neon.o unzip.o util.o plat_sf3000.o hwdisp.o"
+OBJS="libpicofe/input.o libpicofe/in_sdl.o libpicofe/linux/in_evdev.o libpicofe/linux/plat.o libpicofe/fonts.o libpicofe/readpng.o libpicofe/config_file.o cheat.o config.o content.o core.o menu.o menu_font.o i18n.o main.o options.o overrides.o patch.o scale.o scaler_neon.o unzip.o util.o frogui_settings.o plat_sf3000.o hwdisp.o"
 
 cd /home/tomaszz/sf3000-work/picoarch
 
-for src in libpicofe/input.c libpicofe/in_sdl.c libpicofe/linux/in_evdev.c libpicofe/linux/plat.c libpicofe/fonts.c libpicofe/readpng.c libpicofe/config_file.c cheat.c config.c content.c core.c menu.c menu_font.c main.c options.c overrides.c patch.c scale.c scaler_neon.c unzip.c util.c; do
+for src in libpicofe/input.c libpicofe/in_sdl.c libpicofe/linux/in_evdev.c libpicofe/linux/plat.c libpicofe/fonts.c libpicofe/readpng.c libpicofe/config_file.c cheat.c config.c content.c core.c menu.c menu_font.c main.c options.c overrides.c patch.c scale.c scaler_neon.c unzip.c util.c frogui_settings.c; do
     obj="${src%.c}.o"
     $CC $CFLAGS "${DEFINES[@]}" -c -o "$obj" "$src"
 done
